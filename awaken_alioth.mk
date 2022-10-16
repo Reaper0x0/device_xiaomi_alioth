e1fa8442668bc8aa6ff52e0525145c206f0a4dd4
Reaper0x0 <bhuvighate26@gmail.com>
2022-10-16 13:06:27 +0000

    alioth: Awaken Bringup

--- awaken_alioth.mk
+++ awaken_alioth.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The AwakenOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,13 +8,13 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common awaken stuff.
+$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := lineage_alioth
+PRODUCT_NAME := awaken_alioth
 TARGET_BOOT_ANIMATION_RES := 1080
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
